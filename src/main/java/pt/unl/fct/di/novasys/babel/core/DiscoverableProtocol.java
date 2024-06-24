7c515c21c2d0ed9b52832ad603466783ce44c196
Rafael Matos <rafael.dom.matos@gmail.com>
2024-06-24 14:40:05 +0100

    Delete dead code and reformat comments

--- src/main/java/pt/unl/fct/di/novasys/babel/core/DiscoverableProtocol.java
+++ src/main/java/pt/unl/fct/di/novasys/babel/core/DiscoverableProtocol.java
@@ -11,9 +11,9 @@ public abstract class DiscoverableProtocol extends GenericProtocol {
 
     public DiscoverableProtocol(String protoName, short protoId) {
         super(protoName, protoId);
-        this.myself = null;   
+        this.myself = null;
     }
-    
+
     public DiscoverableProtocol(String protoName, short protoId, Host myself) {
         super(protoName, protoId);
         this.myself = myself;
@@ -21,63 +21,62 @@ public abstract class DiscoverableProtocol extends GenericProtocol {
 
     public DiscoverableProtocol(String protoName, short protoId, BlockingQueue<InternalEvent> policy) {
         super(protoName, protoId, policy);
-        this.myself = null;   
+        this.myself = null;
     }
 
     public DiscoverableProtocol(String protoName, short protoId, Host myself, BlockingQueue<InternalEvent> policy) {
         super(protoName, protoId, policy);
         this.myself = myself;
     }
-    
+
     /**
-     * Starts the protocol. Should be implemented to start communication once all the
-     * parameters have been defined.
+     * Starts the protocol. Should be implemented to start communication once all
+     * the parameters have been defined.
      * 
      * Do not evoke directly.
      */
     public abstract void start();
 
     /**
-     * This method is mandatory to be implemented by any protocol that extends the DiscoverabbleProtocol
-     * and it provides information to the Babel core if there is enough information within the protocol
-     * to allow the core to start the protocol execution thread. No event will be processed by the protocol
+     * This method is mandatory to be implemented by any protocol that extends the
+     * DiscoverabbleProtocol and it provides information to the Babel core if there
+     * is enough information within the protocol to allow the core to start the
+     * protocol execution thread. No event will be processed by the protocol
      * until this becomes true.
      * 
      * After becoming true. this function cannot return false again.
+     * 
      * @return true if protocol can start execution
      */
     public abstract boolean readyToStart();
 
     /**
-     * This method must be implemented by any protocol that extends the DiscoverableProtocol.
-     * It should return true while the protocol is still requesting to get information
-     * still needs to obtain contacts form the network. After changing to false it cannot 
-     * change back to true
+     * This method must be implemented by any protocol that extends the
+     * DiscoverableProtocol. It should return true while the protocol is still
+     * requesting to get information still needs to obtain contacts form the
+     * network. After changing to false it cannot change back to true
+     * 
      * @return true if requires contacts to be obtained from the network
      */
-	public abstract boolean needsDiscovery();
-    
-	/**
-	 * This method is used by the DiscoveryProtocol to provide information about contacts (i.e., other
-	 * nodes that are running this protocol) in the network/Internet. While the discovery protocol should
-	 * never provide the identifier of the local host to this protocol, it might provide the same host 
-	 * over time, and hence the protocol should be prepared to deal with that.
-	 * @param host
-	 */
+    public abstract boolean needsDiscovery();
+
+    /**
+     * This method is used by the DiscoveryProtocol to provide information about
+     * contacts (i.e., other nodes that are running this protocol) in the
+     * network/Internet. While the discovery protocol should
+     * never provide the identifier of the local host to this protocol, it might
+     * provide the same host over time, and hence the protocol should be prepared to
+     * deal with that.
+     * 
+     * @param host
+     */
     public abstract void addContact(Host host);
-    
+
     protected final void setMyself(Host h) {
-    	this.myself = h;
-    }
-    
-    public Host getMyself() {
-        return this.myself;
+        this.myself = h;
     }
 
-    @Override
-    void deliverInternalEvent(InternalEvent event) {
-        //if (readyToStart()) {
-            super.deliverInternalEvent(event);
-        //}
+    public Host getMyself() {
+        return this.myself;
     }
 }
